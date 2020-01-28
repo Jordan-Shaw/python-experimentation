81a63451a4f5ecbc75c605411e4705ea26e8dcee
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-01-28 08:48:59 +0000

    Added functionality which takes photo via webcam every unit time

--- face_detection.py
+++ face_detection.py
@@ -1,48 +1,73 @@
-#used for turning img into an array of smaller things to iterate over
 import numpy as np
-
-#openCV stuff
 import cv2
-
-#the graphing stuff which will display img
 import matplotlib.pyplot as plt
-
-#used for getting current directory, should make path work on other machines
 import os
+import json
+from fer import FER
+import tensorflow as tf
+import time 
+
+tf.ConfigProto = tf.compat.v1.ConfigProto
+tf.Session = tf.compat.v1.Session
+
+detector = FER()
+
+
+while True:
+  webcam = cv2.VideoCapture(0)
+  check, frame = webcam.read()
+  print(check)
+  cv2.imwrite('captured_img.jpg', frame)
+  webcam.release()
+  # assigns image to variable img
+  img = cv2.imread('captured_img.jpg')
+
+  img_copy = img.copy()
+
+  #converts img to grayscale
+  img_gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
+
+  #function that should convert back into colour when invoked, isn't currently working
+  def convertToRGB(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+  #assigns the classifier (which is the instructions of what to look for in img) to a variable
+  haar_cascade_face = cv2.CascadeClassifier(
+      os.getcwd() + '/data/haarcascade_frontalface_default.xml')
+
+  #invokes cascade on img
+  faces_rects = haar_cascade_face.detectMultiScale(img_gray, scaleFactor=1.8, minNeighbors=5)
+
+  #prints number of faces found to console
+  print('Faces found: ', len(faces_rects))
+
+  #draws rectangle around face
+  for (x,y,w,h) in faces_rects:
+    cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+  detector_output = detector.detect_emotions(img_copy)
+
+
+  output_dict = {"face_present": "", "emotions": {}}
+
+  if len(faces_rects) > 0:
+    output_dict["face_present"] = True
+    output_dict["emotions"] = str(detector_output[0]["emotions"])
+
+  json_string = json.dumps(output_dict)
+
+  with open('json_file.json', 'w') as f:
+    json.dump(output_dict, f)
+
+  time.sleep(5.0)
 
 
 
-# assigns image to variable img
-img = cv2.imread('barack.jpg')
 
-img_copy = img.copy()
 
-#converts img to grayscale
-img_gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
 
-#function that should convert back into colour when invoked, isn't currently working
-def convertToRGB(image):
-  return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-#assigns the classifier (which is the instructions of what to look for in img) to a variable
-haar_cascade_face = cv2.CascadeClassifier(
-    os.getcwd() + '/data/haarcascade_frontalface_default.xml')
-
-#invokes cascade on img
-faces_rects = haar_cascade_face.detectMultiScale(img_gray, scaleFactor=1.7, minNeighbors=5)
-
-#prints number of faces found to console
-print('Faces found: ', len(faces_rects))
-
-#draws rectangle around face
-for (x,y,w,h) in faces_rects:
-  cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-#should convert img back to color and assign it to recoloured
-#recoloured = convertToRGB(img_gray)
-
-#plt.imshow(convertToRGB(img_gray))
 
 #displays img
-plt.imshow(convertToRGB(img_copy))
-plt.show()
+# plt.imshow(convertToRGB(img_copy))
+# plt.show()
